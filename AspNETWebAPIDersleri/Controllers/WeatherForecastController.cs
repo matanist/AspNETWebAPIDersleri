5828eb5f75ab5fa93508ff0ba35b67d08376d0a2
Fatih BAYTAR <matanist@hotmail.com>
2023-11-08 17:07:11 +0000

    Add project files.

---
+++ AspNETWebAPIDersleri/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNETWebAPIDersleri.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        [HttpGet("ReturnMyName")]
+        public string ReturnMyName()
+        {
+            return "Fatih BAYTAR";
+        }
+
+        [HttpPost]
+        public string Post(int sayi1)
+        {
+            return (sayi1 * sayi1).ToString();
+        }
+    }
+}
