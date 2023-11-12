9dcc55bb2cba332ce53bf6ee66e34bb9ba49be21
Fatih BAYTAR <matanist@hotmail.com>
2023-11-12 12:52:54 +0000

    2. Video için dosyalar

---
+++ AspNETWebAPIDersleri/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using AspNETWebAPIDersleri.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNETWebAPIDersleri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost()]
+        public ResponseModel Login(LoginModel loginModel)
+        {
+
+            if (loginModel.Username == "fatih" && loginModel.Password == "123")
+            {
+                return new ResponseModel()
+                {
+                    Message = "Login işlemi başarılı",
+                    Code = "200",
+                    Data = null
+                };
+
+            }
+
+            return new ResponseModel()
+            {
+                Message = "Login işlemi başarısız",
+                Code = "401",
+                Data = null
+            };
+
+        }
+    }
+}
---
+++ AspNETWebAPIDersleri/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNETWebAPIDersleri.Models
+{
+    public class ResponseModel
+    {
+        public string Message { get; set; }
+        public string Code { get; set; }
+        public object Data { get; set; }
+    }
+}
